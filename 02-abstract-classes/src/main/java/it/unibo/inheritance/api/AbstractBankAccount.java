c76690e6f4e0b9eb7f986511e4a0dce1a856e2d6
Jonathan Crescentini <jonathan.crescentini@studio.unibo.it>
2025-11-11 11:58:42 +0100

    added all methods

--- 02-abstract-classes/src/main/java/it/unibo/inheritance/api/AbstractBankAccount.java
+++ 02-abstract-classes/src/main/java/it/unibo/inheritance/api/AbstractBankAccount.java
@@ -1,11 +1,14 @@
 package it.unibo.inheritance.api;
 
 
+import it.unibo.inheritance.impl.SimpleBankAccount;
+
 abstract class AbstractBankAccount implements BankAccount {
 
     //variables
     private static final double ATM_TRANSACTION_FEE = 1;
     private static final double MANAGEMENT_FEE = 5;
+    private static final double TRANSACTION_FEE = 0.1;
 
     private final AccountHolder holder;
     private double balance;
@@ -19,29 +22,84 @@ abstract class AbstractBankAccount implements BankAccount {
     }
 
 
+    //getters-setters
     @Override
-    void chargeManagementFees(int id){
+    public double getBalance() {
+        return this.balance;
+    }
 
-    };
+    protected void setBalance(final double balance) {
+        this.balance = balance;
+    }
 
-    void deposit(int id, double amount);
+    @Override
+    public int getTransactionsCount() {
+        return this.transactions;
+    }
 
-    void depositFromATM(int id, double amount);
+    protected void resetTransactions() {
+        this.transactions = 0;
+    }
 
-    AccountHolder getAccountHolder();
+    protected void incrementTransactions() {
+        this.transactions++;
+    }
 
-    double getBalance();
+    @Override
+    public AccountHolder getAccountHolder() {
+        return holder;
+    }
 
-    int getTransactionsCount();
 
-    void withdraw(int id, double amount);
-
-    void withdrawFromATM(int id, double amount);
-
+    //booleans
     protected boolean isWithdrawAllowed(final double amount) {
         return getBalance() > amount;
     }
-    
+
+    protected boolean checkUser(final int id) {
+        return this.getAccountHolder().getUserID() == id;
+    }
+
+
+    //methods
+    private void transactionOp(final int id, final double amount) {
+        if (checkUser(id)) {
+            this.balance += amount;
+            this.incrementTransactions();
+        }
+    }
+
+    @Override
+    public void chargeManagementFees(final int usrID) {
+        final double feeAmount = MANAGEMENT_FEE + getTransactionsCount() * TRANSACTION_FEE;
+        if (checkUser(usrID) && isWithdrawAllowed(feeAmount)) {
+            setBalance(getBalance() - feeAmount);
+            resetTransactions();
+        }
+    }
+
+    @Override
+    public void deposit(final int id, final double amount) {
+        this.transactionOp(id, amount);
+    }
+
+    @Override
+    public void depositFromATM(final int id, final double amount) {
+
+        this.deposit(id, amount - ATM_TRANSACTION_FEE);
+    }
+    public void withdraw(final int usrID, final double amount) {
+        if (isWithdrawAllowed(amount)) {
+            this.withdraw(usrID, amount);
+        }
+    }
+
+    public void withdrawFromATM(final int id, final double amount) {
+        this.withdraw(id, amount + ATM_TRANSACTION_FEE);
+    }
+
+
+
 
 
 }
